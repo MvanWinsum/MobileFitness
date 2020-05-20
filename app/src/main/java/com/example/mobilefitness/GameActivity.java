3e2bf66bffe7af4745cf15a796f1a5ff5a38055b
Mitchell van Winsum <mitchell.van.winsum@plat4mation.com>
2020-05-20 17:13:29 +0200

    Merge conflicting changes and convert Kotlin to Java

--- app/src/main/java/com/example/mobilefitness/GameActivity.java
+++ app/src/main/java/com/example/mobilefitness/GameActivity.java
@@ -3,12 +3,22 @@ package com.example.mobilefitness;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
 
 public class GameActivity extends AppCompatActivity {
+
+    DisplayMetrics dm;
+    WindowManager windowManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new GameView(this));
+        dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        Constants.SCREEN_HEIGHT = dm.heightPixels;
+        Constants.SCREEN_WIDTH = dm.widthPixels;
     }
 }
